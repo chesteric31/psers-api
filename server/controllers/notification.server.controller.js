8b200c41b69b85d7577f12587880357feffdcf6f
chesteric31 <chesteric31@gmail.com>
2017-09-24 14:59:01 +0200

    add new method: notify specific user with title of show

--- server/controllers/notification.server.controller.js
+++ server/controllers/notification.server.controller.js
@@ -51,7 +51,8 @@ console.log(show_title);
     });
 
     var userId = req.params.user_id;
-    console.log(req.params);
+    console.log("params: " + req.params);
+    console.log(req.params.user_id);
     var sender = new gcm.Sender(secrets.fcm);
     sender.send(message, { registrationTokens: user_id }, function (err, response) {
       if (err) {
