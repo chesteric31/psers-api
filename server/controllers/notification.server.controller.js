dc99f74de7c1aabf32e7de73304c84d47b248eb4
chesteric31 <chesteric31@gmail.com>
2017-11-03 12:28:58 +0100

    change notify users methods

--- server/controllers/notification.server.controller.js
+++ server/controllers/notification.server.controller.js
@@ -8,46 +8,18 @@ webPush.setGCMAPIKey(secrets.fcm);
 
 module.exports = {
 
-  notifyUsers: function(req, res){
-
-    var sender = new gcm.Sender(secrets.fcm);
+  notifyUsers: function(req, res) {
     var show_title = "Star trek";
-
-    // Prepare a message to be sent
-    var message = new gcm.Message({
-        notification: {
-          title: "Hi, new update for " + show_title,
-          icon: "ic_launcher",
-          body: "Click to see the latest episodes"
-        }
-    });
-
     User.find({}, function(err, users) {
-
-      // user subscription ids to deliver message to
-      var user_ids = _.map(users, 'user_id');
-
-      console.log("User Ids", user_ids);
-
-      console.log(sender);
-
-      // Actually send the message
-      /*sender.send(message, { registrationTokens: user_ids }, function (err, response) {
-        if (err) {
-            console.error(err);
-        } else {
-          return res.json(response);
-        } 
-      });*/
-      setTimeout(function() {
+        for (var user in users) {
             webPush.sendNotification({
-              endpoint: req.body.endpoint,
-              TTL: req.body.ttl,
+              endpoint: user.endpoint,
+              TTL: 1,
               keys: {
-                p256dh: req.body.key,
-                auth: req.body.authSecret
+                p256dh: user.key,
+                auth: user.authSecret
               }
-            }, req.body.payload)
+            }, show_title)
             .then(function() {
               res.sendStatus(201);
             })
@@ -55,9 +27,8 @@ module.exports = {
               console.log(error);
               res.sendStatus(500);
             });
-          }, req.body.delay * 1000);
-        });
-   
+        }
+    });
   },
 
   notifyUser: function(req, res) {
