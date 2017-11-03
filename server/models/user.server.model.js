e0cc662af7f14b2f8cd3f3ef6d657fea984f4d9b
chesteric31 <chesteric31@gmail.com>
2017-11-03 11:24:10 +0100

    test with web push notification payload

--- server/models/user.server.model.js
+++ server/models/user.server.model.js
@@ -2,9 +2,12 @@ var mongoose          = require('mongoose'),
     bcrypt            = require('bcrypt'),
     userSchema        = 
     	mongoose.Schema({
-    						user_id:       { type: String, required: true, unique: true},
+    						user_id:       { type: String, required: true, unique: true },
     						registered_on: { type: Date, default: Date.now },
-    						watching_shows_tvmaze_ids: [{ type: Number }]
+    						watching_shows_tvmaze_ids: [{ type: Number }],
+                            endpoint: { type: String, required: true },
+                            key: { type: String, required: true },
+                            authSecret: { type: String, required: true }
 		});
 
 module.exports = mongoose.model('User', userSchema, 'users');
